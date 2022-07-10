943cfd7894fd3a2eb59d05fb376be39279a33b85
queenhellen <hellendongyun@gmail.com>
2022-07-11 01:10:02 +1200

    add click to buttons
    add click to buttons

---
+++ src/constants/items.js
@@ -0,0 +1,29 @@
+const items = {products:
+        [
+            {
+                name: 'Sledgehammer',
+                price: 125.76,
+                id: '1',
+            },
+            {
+                name: 'Axe',
+                price: 190.51,
+                id:'2',
+            },
+            {
+                name: 'Bandsaw',
+                price: 562.14,
+                id:'3',
+            },
+            {
+                name: 'Chisel',
+                price: 13.9,
+                id:'4',
+            },
+            {
+                name: 'Hacksaw',
+                price: 19.45,
+                id:'5',
+            }
+        ]};
+export default items;
